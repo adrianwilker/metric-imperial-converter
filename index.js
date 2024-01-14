e61ab83664b96a84a9e57964db058a991b92de92
Adrian Wilker <56568057+adrianwilker@users.noreply.github.com>
2024-01-14 12:43:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,59 @@
-const express = require('express')
+'use strict';
 
-const app = express()
-const PORT = 4000
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const expect      = require('chai').expect;
+const cors        = require('cors');
+require('dotenv').config();
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
+const apiRoutes         = require('./routes/api.js');
+const fccTestingRoutes  = require('./routes/fcctesting.js');
+const runner            = require('./test-runner');
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
+let app = express();
 
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
+app.use('/public', express.static(process.cwd() + '/public'));
 
-// Export the Express API
-module.exports = app
+app.use(cors({origin: '*'})); //For FCC testing purposes only
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Index page (static HTML)
+app.route('/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+//For FCC testing purposes
+fccTestingRoutes(app);
+
+//Routing for API 
+apiRoutes(app);  
+    
+//404 Not Found Middleware
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
+
+const port = process.env.PORT || 3000;
+
+//Start our server and tests!
+app.listen(port, function () {
+  console.log("Listening on port " + port);
+  if(process.env.NODE_ENV==='test') {
+    console.log('Running Tests...');
+    setTimeout(function () {
+      try {
+        runner.run();
+      } catch(e) {
+          console.log('Tests are not valid:');
+          console.error(e);
+      }
+    }, 1500);
+  }
+});
+
+module.exports = app; //for testing
