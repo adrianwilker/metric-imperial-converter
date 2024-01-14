d49872f55110b73167ed6c611e51375c155695f4
Adrian Wilker <56568057+adrianwilker@users.noreply.github.com>
2024-01-14 12:55:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 
 const express     = require('express');
 const bodyParser  = require('body-parser');
-const expect      = require('chai').expect;
-const cors        = require('cors');
 require('dotenv').config();
 
 const apiRoutes         = require('./routes/api.js');
@@ -14,8 +12,6 @@ let app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -25,9 +21,6 @@ app.route('/')
     res.sendFile(process.cwd() + '/views/index.html');
   });
 
-//For FCC testing purposes
-fccTestingRoutes(app);
-
 //Routing for API 
 apiRoutes(app);  
     
@@ -36,24 +29,6 @@ app.use(function(req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
-});
-
-const port = process.env.PORT || 3000;
-
-//Start our server and tests!
-app.listen(port, function () {
-  console.log("Listening on port " + port);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-          console.log('Tests are not valid:');
-          console.error(e);
-      }
-    }, 1500);
-  }
 });
 
 module.exports = app; //for testing
